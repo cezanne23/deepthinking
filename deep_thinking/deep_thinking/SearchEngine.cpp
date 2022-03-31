6ae2489f059aeb9886b74175f0231f04f68f49f7
HunSoo Lee <ensemble.lee@gmail.com>
2022-04-01 04:20:15 +0900

    [Feature] SearchEngine class definition  - Development: SearchEngine class prototype  - TC: SearchEngine not initilized TC

---
+++ deep_thinking/deep_thinking/SearchEngine.cpp
@@ -0,0 +1,11 @@
+
+#include "SearchEngine.h"
+
+vector<int> SearchEngine::seachID(const map<int, EmployeeInfo>& employeeInfo,
+                                  const string condition) {
+  vector<int> result;
+  // for tdd
+  // result.push_back(1);
+
+  return result;
+}
