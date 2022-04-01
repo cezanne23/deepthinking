76e6739575a84c259b132354f7c4d1052e198a97
YeonJu Lee <cezanne.lee@samsung.com>
2022-04-01 11:30:05 +0900

    [FEATURE] Add EmployeeManager class

---
+++ deep_thinking/deep_thinking/EmployeeManager.cpp
@@ -0,0 +1,23 @@
+#include "EmployeeManager.h"
+
+const string EmployeeManager::fileExtension(string filepath) const {
+  if (filepath.find_last_of(".") != std::string::npos)
+    return filepath.substr(filepath.find_last_of(".") + 1);
+  return "";
+}
+
+bool EmployeeManager::setFilePath(string inputfile, string outputfile) {
+  if (!isVaildFilePath(inputfile)) return false;
+  if (!isVaildFilePath(outputfile)) return false;
+
+  intputfile_ = inputfile;
+  outputfile_ = outputfile;
+  return true;
+}
+
+const bool EmployeeManager::isVaildFilePath(string filepath) const {
+  if (filepath.find("./") != string::npos) return false;
+  if (fileExtension(filepath) != "txt") return false;
+
+  return true;
+}
