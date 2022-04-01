66b4c8e4af16b5aa93b816814d09ed455c66512f
YeonJu Lee <cezanne.lee@samsung.com>
2022-04-01 16:09:50 +0900

    [FEATURE] Add file read/write function

--- deep_thinking/deep_thinking/EmployeeManager.cpp
+++ deep_thinking/deep_thinking/EmployeeManager.cpp
@@ -1,5 +1,8 @@
 #include "EmployeeManager.h"
 
+#include <fstream>
+#include <iostream>
+
 const string EmployeeManager::fileExtension(string filepath) const {
     if (filepath.find_last_of(".") != std::string::npos)
         return filepath.substr(filepath.find_last_of(".") + 1);
@@ -20,4 +23,33 @@ const bool EmployeeManager::isVaildFilePath(string filepath) const {
     if (fileExtension(filepath) != "txt") return false;
 
     return true;
+}
+
+vector<string> EmployeeManager::execute() {
+    ifstream inputfile(intputfile_);
+
+    // TODO : integration EmployeeInforManager - TDD checking
+    vector<string> lines;
+    string line;
+    //
+    if (!inputfile.is_open()) {
+        cerr << "Could not open the file !! " << intputfile_ << "'" << endl;
+        throw std::invalid_argument("it's invalid filePath");
+    }
+
+    int count = 0;
+    while (std::getline(inputfile, line) && count < 10) {
+        // TODO : integration EmployeeInforManager - TDD  checking
+        lines.push_back(line);
+        count++;
+        //
+        // TOBE: ofstream && writeCommandResult(ouput_file, ..); 
+    }
+
+    inputfile.close();
+    return lines;
+}
+
+void EmployeeManager::writeCommandResult(ofstream& ouptputfile, string result) {
+    ouptputfile << result << endl;
 }
