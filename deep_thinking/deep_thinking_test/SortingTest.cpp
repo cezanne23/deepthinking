82626a211621d67288b7137d0eb032702f5ed512
YeonJu Lee <cezanne.lee@samsung.com>
2022-04-05 20:15:18 +0900

    [Performance]  update sorting logic for -p option

--- deep_thinking/deep_thinking_test/SortingTest.cpp
+++ deep_thinking/deep_thinking_test/SortingTest.cpp
@@ -2,33 +2,15 @@
 #include "../deep_thinking/Sorting.h"
 
 TEST(SortingTest, PriorityQueueTest1) {
-    vector<EmployeeInfo> employeeList = {
-        { "80030189","XVDD CHUNG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "70040589","SDF LEE", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "99043189","EGKD ONG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "02045189","BKD NAMGUNG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "03980189","FDKFU OH", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "00070189","BZKCI KYE", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "01140189","PODNH GUN", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "10890189","KDVJN GOH", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69041189","ASF KIM", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "15034289","VKDIE JIN", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "21230189","ACGKE JANG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "90020189","VCXVD KOO", "CL3", "010-1221-5555", "19891210", "ADV" } };
-
-    vector<EmployeeInfo> topFiveList = {
-        { "69041189","ASF KIM", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "70040589","SDF LEE", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "80030189","XVDD CHUNG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "90020189","VCXVD KOO", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "99043189","EGKD ONG", "CL3", "010-1221-5555", "19891210", "ADV" },
-    };
+    vector<string> employeeNumList = { "80030189","70040589","99043189","02045189", "03980189", "00070189", "01140189", "10890189",
+                                       "69041189","15034289","21230189","90020189" };
+    vector<string> topFiveList = { "69041189","70040589","80030189","90020189","99043189" };
 
     PriorityQueue pq{};
-    vector<EmployeeInfo> results{};
+    vector<string> results{};
     int index = 0;
 
-    pq.sort(employeeList);
+    pq.sort(employeeNumList);
     results = pq.getTopk();
 
     for (auto& result : results) {
@@ -37,33 +19,15 @@ TEST(SortingTest, PriorityQueueTest1) {
 }
 
 TEST(SortingTest, PriorityQueueTest2) {
-    vector<EmployeeInfo> employeeList = {
-        { "69000015","XVDD CHUNG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000014","SDF LEE", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000013","EGKD ONG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000012","BKD NAMGUNG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000011","FDKFU OH", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000010","BZKCI KYE", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000009","PODNH GUN", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000008","KDVJN GOH", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000007","ASF KIM", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000006","VKDIE JIN", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000005","ACGKE JANG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000004","VCXVD KOO", "CL3", "010-1221-5555", "19891210", "ADV" } };
-
-    vector<EmployeeInfo> topFiveList = {
-        { "69000004","VCXVD KOO", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000005","ACGKE JANG", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000006","VKDIE JIN", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000007","ASF KIM", "CL3", "010-1221-5555", "19891210", "ADV" },
-        { "69000008","KDVJN GOH", "CL3", "010-1221-5555", "19891210", "ADV" },
-    };
+    vector<string> employeeNumList = { "69000015", "69000014", "69000013", "69000012", "69000011", "69000010",
+                                       "69000009", "69000008", "69000007", "69000006", "69000005", "69000004" };
+    vector<string> topFiveList = { "69000004","69000005","69000006","69000007","69000008" };
 
     PriorityQueue pq{};
-    vector<EmployeeInfo> results{};
+    vector<string> results{};
     int index = 0;
 
-    pq.sort(employeeList);
+    pq.sort(employeeNumList);
     results = pq.getTopk();
 
     for (auto& result : results) {
